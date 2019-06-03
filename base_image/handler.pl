0c8ebeebecc6fa91c9b000b35b6e549f39cc9566
marlon.buella <marlon.b.buella@gmail.com>
2019-06-04 00:10:26 +0800

    Implement action response + interaction with PHP

--- base_image/handler.pl
+++ base_image/handler.pl
@@ -1,19 +1,50 @@
-use JSON;
+use IPC::Run qw(run);
+use Encode 'encode';
 use JSON::Create create_json;
 
 sub main
 {
     my @params = @ARGV[0] ? decode_json(@ARGV[0]) : {};
 
-    my $output = "Hello world!";
+    my $scriptPath = '/var/www/index.php';
+    my $requestBody = '';
 
-    # logs
-    printf("This is an example log message from an arbitrary Perl program!\n");
+    # build php environment vars
+    my %php_env_vars = (
+        'REDIRECT_STATUS' => 200,
+        'REQUEST_METHOD' => 'GET',
+        'SCRIPT_FILENAME' => $scriptPath,
+        'SCRIPT_NAME' => '/index.php',
+        'PATH_INFO' => '/',
+        'SERVER_NAME' => 'MBUELLA',
+        'SERVER_PROTOCOL' => 'HTTP/1.1',
+        'REQUEST_URI' => '/',
+        'QUERY_STRING' => '',
+        'CONTENT_LENGTH' => length(encode('UTF-8', $requestBody)),
+        'HTTPS' => true
+    );
 
+    foreach $variable (keys %php_env_vars) {
+        $ENV{$variable} = $php_env_vars{$variable};
+    }
+    
+    # execute php file
+    run ["php-cgi", "-f", "index.php"], ">", \my $output;
+
+    # parse response
+    my ($headerString, $body) = split /\R\R/, $output;
+    my @headerLines = split /\R/, $headerString;
+    my %headers = ();
+    foreach my $line (@headerLines) {
+        my ($key, $val) = split /: /, $line;
+        $headers{$key} = $val;
+    }
+    
     # output
     my %response = (
-        params => @params,
-        msg => $output
+        statusCode => 200,
+        headers => \%headers,
+        body => sprintf("%s", $body),
     );
 
     return create_json(\%response);
