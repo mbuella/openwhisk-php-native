bfb406f9bfc4201a969a8ca111661c3bfaf43090
marlon.buella <marlon.b.buella@gmail.com>
2019-06-08 23:42:05 +0800

    Pass request variables to PHP runtime

--- base_image/handler.pl
+++ base_image/handler.pl
@@ -5,28 +5,39 @@ use JSON::Create create_json;
 
 sub main
 {
-    my @params = @ARGV[0] ? decode_json(@ARGV[0]) : {};
+    my $params = @ARGV[0] ? decode_json(@ARGV[0]) : {};
 
+    my $httpHeaders = $params->{'__ow_headers'};
+    my $requestMethod = uc $params->{'__ow_method'} or 'GET';
+    my $requestUri = $params->{'__ow_path'} or '/';
     my $scriptPath = '/var/www/index.php';
+    my $queryString = $params->{'__ow_query'} or '';
     my $requestBody = '';
 
     # build php environment vars
     my %php_env_vars = (
         'REDIRECT_STATUS' => 200,
-        'REQUEST_METHOD' => 'GET',
+        'REQUEST_METHOD' => $requestMethod,
         'SCRIPT_FILENAME' => $scriptPath,
         'SCRIPT_NAME' => '/index.php',
         'PATH_INFO' => '/',
         'SERVER_NAME' => 'MBUELLA',
         'SERVER_PROTOCOL' => 'HTTP/1.1',
-        'REQUEST_URI' => '/',
-        'QUERY_STRING' => '',
+        'REQUEST_URI' => $requestUri,
+        'QUERY_STRING' => $queryString,
         'CONTENT_LENGTH' => length(encode('UTF-8', $requestBody)),
         'HTTPS' => true
     );
 
     foreach $variable (keys %php_env_vars) {
         $ENV{$variable} = $php_env_vars{$variable};
+    }
+    
+    foreach $headerKey (keys %{$httpHeaders}) {
+        $oldKey = $headerKey;
+        $headerKey =~ tr/-/_/;
+        $ENV{'HTTP_' . uc($headerKey)} = $httpHeaders->{$oldKey};
+        $ENV{uc($headerKey)} = $httpHeaders->{$oldKey};
     }
 
     my %headers = ();
@@ -62,7 +73,7 @@ sub main
         headers => \%headers,
         body => sprintf("%s", $body),
     );
-    
+
     return create_json(\%response);
 }
 
